5996d7213801903ce4b46bc41bd837bfc84a6016
Evgeniy <49danon49@gmail.com>
2019-12-04 20:27:32 +0300

    layaot banner

---
+++ index.js
@@ -0,0 +1,17 @@
+const options = {
+    threshold: [0.5]
+  };
+  
+  const observer = new IntersectionObserver(entry => {
+    entry.forEach(change => {
+      console.log(change.isIntersecting);
+  
+      if (change.isIntersecting) {
+        change.target.classList.add("visible");
+        
+      }
+    });
+  }, options);
+  
+  [...document.querySelectorAll(".banner__text")].forEach(elm => observer.observe(elm));
+  
