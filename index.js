28ffd19402f1492b776e95c879920b342c2a2d78
Evgeniy <49danon49@gmail.com>
2019-12-05 15:40:10 +0300

    fix style media-queries

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const options = {
-    threshold: [0.5]
-  };
+// const options = {
+//     threshold: [0.5]
+//   };
   
-  const observer = new IntersectionObserver(entry => {
-    entry.forEach(change => {
-      console.log(change.isIntersecting);
+//   const observer = new IntersectionObserver(entry => {
+//     entry.forEach(change => {
+//       console.log(change.isIntersecting);
   
-      if (change.isIntersecting) {
-        change.target.classList.add("visible");
+//       if (change.isIntersecting) {
+//         change.target.classList.add("visible");
         
-      }
-    });
-  }, options);
+//       }
+//     });
+//   }, options);
   
-  [...document.querySelectorAll(".banner__text")].forEach(elm => observer.observe(elm));
+//   [...document.querySelectorAll(".banner__text")].forEach(elm => observer.observe(elm));
   
